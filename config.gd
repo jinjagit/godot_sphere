b551676b2661da9778a5a8c81df129e36c719b08
Simon Tharby <simon@liquidvoting.io>
2022-02-19 18:08:02 +0000

    procedural sphere

---
+++ config.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+func _init():
+	VisualServer.set_debug_generate_wireframes(true)
+
+func _input(event):
+			
+	if event is InputEventKey and Input.is_key_pressed(KEY_P):
+		var vp = get_viewport()
+		vp.debug_draw = (vp.debug_draw + 1 ) % 4
